65b1939a44001ce34a476cc81d8f355271ca0ccb
Charlie K <charlie.kern01@gmail.com>
2020-03-27 17:55:50 -0700

    Re-add migration

--- migrations/2018_04_25_000000_create_guardian_footprint_table.php
+++ migrations/2018_04_25_000000_create_guardian_footprint_table.php
@@ -16,6 +16,7 @@ return Migration::createTable(
         $table->string('user_agent')->nullable();
         $table->boolean('do_not_track')->default(0);
         $table->string('timezone')->nullable();
+        $table->string('country')->nullable();
         $table->string('operating_system')->nullable();
         $table->string('device')->nullable();
         $table->string('browser')->nullable();
@@ -28,6 +29,7 @@ return Migration::createTable(
         // event information
         $table->string('event');
         $table->integer('score')->default(0);
+        $table->boolean('is_negative')->default(false);
         $table->integer('since_last_event')->unsigned()->nullable();
 
         // dates
